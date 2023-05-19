627235790d134ef8b128c5bbceeb8f5c40d25785
Emiliano Gusberti <emggw@hotmail.com>
2023-05-18 21:15:24 -0300

    Agrego CSS, creo h1 y container

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,6 +1,11 @@
 const app = document.getElementById("app");
 
-const container = document.createElement("h1");
+const titulo = document.createElement("h1");
+titulo.textContent = "¡Calculame ésta!";
 
-container.textContent = "Hello World!!!";
+// Creo container de toda la calculadora
+const container = document.createElement("div");
+container.classList.add("grid-container");
+
+app.appendChild(titulo);
 app.appendChild(container);
