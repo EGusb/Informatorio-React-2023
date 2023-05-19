d4a50f1682f891c047dcad1af54a3610d83b80dc
Emiliano Gusberti <emggw@hotmail.com>
2023-05-19 11:43:10 -0300

    Modifico estilo, agrego fnc crearNodo

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,11 +1,38 @@
+function agregarNodo(nodoPadre, tag, clases = [], textContent = "") {
+  const nodoHijo = document.createElement(tag);
+  clases.forEach((clase) => nodoHijo.classList.add(clase));
+  nodoHijo.textContent = textContent;
+  nodoPadre.appendChild(nodoHijo);
+  return nodoHijo;
+}
+
 const app = document.getElementById("app");
 
-const titulo = document.createElement("h1");
-titulo.textContent = "¡Calculame ésta!";
+const titulo = agregarNodo(app, "h1", [], (textContent = "¡Calculame ésta!"));
+const container = agregarNodo(app, "div", ["calculadora"]);
+const display = agregarNodo(container, "div", ["display"], "0");
+const containerBotones = agregarNodo(container, "div", ["boton-container"]);
 
-// Creo container de toda la calculadora
-const container = document.createElement("div");
-container.classList.add("grid-container");
+const botones = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0",
+  ".",
+  "AC",
+  "=",
+];
+botones.forEach((num) =>
+  agregarNodo(containerBotones, "button", ["calc-boton"], num)
+);
 
-app.appendChild(titulo);
-app.appendChild(container);
+const operadores = ["+", "-", "x", "/"];
+operadores.forEach((op) =>
+  agregarNodo(containerBotones, "button", ["calc-boton", "operacion"], op)
+);
