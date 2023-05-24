342d97bcccfc78806702a4c55a69915c14fae973
Emiliano Gusberti <emggw@hotmail.com>
2023-05-24 10:30:10 -0300

    Arreglo espaciado

--- DOM/index.js
+++ DOM/index.js
@@ -11,23 +11,23 @@ function getCharacterData() {
     .then((data) => {
       // Obtener la lista de personajes
       const characters = data.results;
-      
+
       // Recorrer la lista de personajes y mostrar los datos en pantalla
       characters.forEach((character) => {
         const { name, species, image } = character;
 
         // Crear elementos HTML para mostrar los datos
         const characterDiv = document.createElement("div");
-		characterDiv.classList.add("character");
+        characterDiv.classList.add("character");
 
         const characterImage = document.createElement("img");
-		characterImage.classList.add("char-img");
+        characterImage.classList.add("char-img");
 
         const characterName = document.createElement("p");
-		characterName.classList.add("char-name");
+        characterName.classList.add("char-name");
 
         const characterSpecies = document.createElement("p");
-		characterSpecies.classList.add("char-species");
+        characterSpecies.classList.add("char-species");
 
         // Establecer los valores de los elementos
         characterImage.src = image;
